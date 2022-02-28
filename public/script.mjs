1492306b6604990479222b4c2c74a77b5cb68c51
LambdAurora <email@lambdaurora.dev>
2022-02-28 10:42:11 +0100

    Improve mobile navigation.

--- public/script.mjs
+++ public/script.mjs
@@ -1,22 +1,35 @@
 for (const sidenav_trigger of document.querySelectorAll("a.ls_sidenav_trigger")) {
 	const target = sidenav_trigger.attributes["ls_data_target"].value;
 	const sidenav = document.getElementById(target);
+	const sidenav_darkened_trigger = document.querySelector(`div.ls_sidenav_darkened[ls_data_target="${target}"]`);
 
-	if (sidenav) {
-		sidenav_trigger.addEventListener("click", e => {
-			let current_display = sidenav.style.display;
+	let last_timeout;
 
-			if (!current_display) {
-				sidenav.style.display = "block";
-				setTimeout(() => {
-					sidenav.style.transform = "translateX(0%)";
-				}, 10);
-			} else {
-				sidenav.style.removeProperty("transform");
-				setTimeout(() => {
-					sidenav.style.removeProperty("display");
-				}, 500);
+	if (sidenav && sidenav_darkened_trigger) {
+		sidenav_darkened_trigger.addEventListener("click", _ => {
+			if (last_timeout) {
+				clearTimeout(last_timeout);
+				last_timeout = undefined;
 			}
+
+			sidenav_darkened_trigger.style.removeProperty("display");
+			sidenav.style.removeProperty("transform");
+			last_timeout = setTimeout(() => {
+				sidenav.style.removeProperty("display");
+			}, 500);
+		});
+
+		sidenav_trigger.addEventListener("click", _ => {
+			if (last_timeout) {
+				clearTimeout(last_timeout);
+				last_timeout = undefined;
+			}
+
+			sidenav_darkened_trigger.style.display = "block";
+			sidenav.style.display = "block";
+			last_timeout = setTimeout(() => {
+				sidenav.style.transform = "translateX(0%)";
+			}, 10);
 		});
 	}
 }
