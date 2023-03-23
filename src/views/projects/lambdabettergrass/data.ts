f296500959c6c70b9db7378a2c2a028998f1a09c
LambdAurora <email@lambdaurora.dev>
2023-03-23 23:23:21 +0100

    Update LBG branch.

--- src/views/projects/lambdabettergrass/data.ts
+++ src/views/projects/lambdabettergrass/data.ts
@@ -1,5 +1,5 @@
 export const title = "LambdaBetterGrass";
-export const BRANCH = "1.19.3";
+export const BRANCH = "1.19.4";
 
 export function get_path(path: string) {
 	return `https://raw.githubusercontent.com/LambdAurora/LambdaBetterGrass/${BRANCH}/${path}`;
