4ae35ad6c209c750bb7cdc146059ba48f5feb8b1
Layla <email@lambdaurora.dev>
2026-05-06 01:09:16 +0200

    Fix my description, add plural art resources.

---
+++ src/views/resources/plurality/art.ts
@@ -0,0 +1,9 @@
+import { ViewSpec } from "../../../engine/view.ts";
+
+export const SPEC: ViewSpec = {
+	page: {
+		title: "Plural Art",
+		description: "Some art surrounding plurality I've found over time.",
+		keywords: ["LambdAurora"],
+	},
+};
